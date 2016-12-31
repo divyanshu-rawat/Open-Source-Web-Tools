335cd4617a2d55843ab00d6e68345299ff5ccd55
divyanshu <divyanshu.r46956@gmail.com>
2017-01-01 02:12:27 +0530

    Implemented KARMA successfully !

---
+++ Linting and Automated Unit Testing/lint/lintTest.js
@@ -0,0 +1,23 @@
+
+// jsLint is a code analysis tool that helps developers find and fix problems in their code.
+// You can customize the code rules that jsLint applies to your code. 
+
+'use strict'; 
+
+function init(){
+
+var recordCount = 3;
+
+if(recordCount === 2)
+{
+    console.log("we have two");
+}
+
+else
+{
+    console.log('not two');
+}
+
+}
+
+init();
