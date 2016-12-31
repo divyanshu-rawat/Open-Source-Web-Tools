3182adc4a4a0be170fd5d5ab0ab66edf4c2d39a7
divyanshu <divyanshu.r46956@gmail.com>
2017-01-01 03:45:52 +0530

    Working with minification and compilation !

---
+++ Minification and Compiling/js/mathService.js
@@ -0,0 +1,22 @@
+var mathService = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+};
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
---
+++ Minification and Compiling/js/minify-html.js
@@ -0,0 +1,1 @@
+var html = '<!DOCTYPE html><html lang="en-US"><head><meta charset="utf-8"><title>Simple Page<link rel="stylesheet" href="styles.css"></head><body><h1>This is a very simple page</h1><img src=./temp.png></body></html>';
