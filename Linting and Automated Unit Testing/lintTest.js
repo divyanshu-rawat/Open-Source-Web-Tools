e1b9cc0e06120e56e3016c091ab3d7ab608d8478
divyanshu <divyanshu.r46956@gmail.com>
2016-12-31 23:57:52 +0530

    Started working on JSlint git add .

---
+++ Linting and Automated Unit Testing/lintTest.js
@@ -0,0 +1,17 @@
+init();
+
+function init(){
+
+    recordCount = 3;
+    
+    if(recordCount == 2)
+    {
+        console.log("we have two");
+	}
+
+	else
+	{
+        console.log('not two')
+	}
+
+}
