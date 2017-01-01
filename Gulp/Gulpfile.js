208e118ce03ac659b8ccc6e15c0447375f357489
divyanshu <divyanshu.r46956@gmail.com>
2017-01-01 17:39:05 +0530

    testing GULP task runner git add .!

---
+++ Gulp/Gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['jshint', 'uglify']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// gulp.task('default', ['jshint', 'sass', 'uglify', 'watch']);
